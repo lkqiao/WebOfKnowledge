711bb264d0ebe7a43c66155159a223fefa376626
Alex <proshkinalexander@gmail.com>
2024-06-22 11:56:56 -0700

    added default react app / added basic GET functionality

--- backend/server.py
+++ backend/server.py
@@ -7,11 +7,16 @@ CORS(app)
 @app.route('/api/data', methods=['GET', 'POST'])
 def data():
     if request.method == 'POST':
-        data = request.get_json()
+        data_request = request.get_json()
         # Process data
-        return jsonify({'message': 'Data received', 'data': data})
+        return jsonify({'message': 'Data received', 'data': data_request})
+
+    if request.method == 'GET':
+        return jsonify({'message': 'Data received', 'data': 'Hello World'})
+
     else:
         return jsonify({'message': 'Send some data'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
